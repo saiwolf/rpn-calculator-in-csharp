3d963197d5cb06cf6ebbc9d662827874f0025438
Robert E. Cato <rcato@ci.thibodaux.la.us>
2026-09-08 16:16:01 -0500

    Commented ScopeStack.cs

--- tests/RPN.Calc.Tests/Logging/ScopeStack.cs
+++ tests/RPN.Calc.Tests/Logging/ScopeStack.cs
@@ -2,12 +2,26 @@
 
 namespace RPN.Calc.Tests.Logging;
 
+/// <summary>
+/// A simple stack of logical scopes that is local to the current async context.
+/// </summary>
 internal static class ScopeStack
 {
+    /// <summary>
+    /// <para>Async-local stack of scopes.</para>
+    /// </summary>
     private static readonly AsyncLocal<ImmutableStack<object>?> _stack = new();
 
+    /// <summary>
+    /// <para>Gets the current stack of scopes or a blank stack if the stack has no values.</para>
+    /// </summary>
     public static ImmutableStack<object> Current = _stack.Value ?? [];
 
+    /// <summary>
+    /// <para>Pushes a new scope onto the stack and returns an <see cref="IDisposable"/> that will pop the scope when disposed.</para>
+    /// </summary>
+    /// <param name="state">The state for the new scope.</param>
+    /// <returns>An <see cref="IDisposable"/> that will pop the scope when disposed.</returns>
     public static IDisposable Push(object state)
     {
         ImmutableStack<object> previous = Current;
@@ -15,6 +29,10 @@ internal static class ScopeStack
         return new PopScope(previous);
     }
 
+    /// <summary>
+    /// <para>An <see cref="IDisposable"/> that pops a scope from the stack when disposed.</para>
+    /// </summary>
+    /// <param name="previous">The previous stack of scopes.</param>
     private sealed class PopScope(ImmutableStack<object> previous) : IDisposable
     {
         private readonly ImmutableStack<object> _previous = previous;
